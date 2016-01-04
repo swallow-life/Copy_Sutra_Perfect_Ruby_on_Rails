b7e81a078b245cb4148d748896ee70221c4259ff
swallow-life <mshk_stt@yahoo.co.jp>
2016-01-04 03:12:11 +0000

    copy sutra 9.9

--- c9/advance/app/models/person.rb
+++ c9/advance/app/models/person.rb
@@ -1,7 +1,37 @@
-#9.6
+#9.6,9.9
 class Person
-	include ActiveModel::Validions
-	attr_accessor :name, :email
+# 	include ActiveModel::Validations
+	#9.9
+	include ActiveModel::AttributeMethods
 
-	validate :email, presence: true, email: true
+	attribute_method_affix({prefix: 'reset_', suffix: '_to_default!'}, {prefix: 'reset_', suffix: '_to_def'})
+# 	attribute_method_affix prefix: 'reset_', suffix: '_to_def'
+	attribute_method_suffix '_contrived?'
+	attribute_method_prefix 'clear_'
+	define_attribute_methods :name
+# 	attr_accessor :name, :email
+	attr_accessor :name
+# 	validate :email, presence: true, email: true
+
+	def attributes
+		{'name' => @name}
+	end
+
+	private
+
+	def attribute_contrived?(attr)
+		true
+	end
+
+	def clear_attribute(attr)
+		send("#{attr}=", nil)
+	end
+
+	def reset_attribute_to_default!(attr)
+		send("#{attr}=", 'Default Name')
+	end
+
+	def reset_attribute_to_def(attr)
+		send("#{attr}=", 'Def')
+	end
 end
