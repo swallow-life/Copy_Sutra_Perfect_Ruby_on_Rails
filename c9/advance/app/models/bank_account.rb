ea66fb1e4aa97c33cefcddcdd0c00f6acc820110
swallow-life <mshk_stt@yahoo.co.jp>
2016-01-27 03:34:30 +0000

    copy sutra 9.34

--- c9/advance/app/models/bank_account.rb
+++ c9/advance/app/models/bank_account.rb
@@ -1,6 +1,8 @@
-#9.3
+#9.3,9.32,9.34
 class BankAccount < ActiveRecord::Base
-	before_save				EncryptionWrapper.new("credit_card_number")
-	after_save				EncryptionWrapper.new("credit_card_number")
-	after_initialize	EncryptionWrapper.new("credit_card_number")
+# 	before_save				EncryptionWrapper.new("credit_card_number")
+# 	after_save				EncryptionWrapper.new("credit_card_number")
+# 	after_initialize	EncryptionWrapper.new("credit_card_number")
+
+	composed_of :balance, class_name: "Money", mapping: [%w(amount amount), %w(currency currency)]
 end
