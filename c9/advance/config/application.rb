fadcad217ed9709d55fcfb72447e57efea883a72
swallow-life <mshk_stt@yahoo.co.jp>
2016-01-28 03:08:18 +0000

    copy sutra 9.35

--- c9/advance/config/application.rb
+++ c9/advance/config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Advance
   class Application < Rails::Application
+		#9.35
+		config.autoloaded_paths << "#{config.root}/app/services"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
