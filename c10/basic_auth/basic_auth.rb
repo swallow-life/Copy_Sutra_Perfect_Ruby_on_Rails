9503692afbf11bbd8fb5c4260daa651198514e08
swallow-life <mshk_stt@yahoo.co.jp>
2016-02-03 03:11:19 +0000

    copy sutra 10.11

--- c10/basic_auth/basic_auth.rb
+++ c10/basic_auth/basic_auth.rb
@@ -7,6 +7,15 @@ class BasicAuth
 	end
 
 	def call(env)
+		#10.11
+		if auth_header = env["HTTP_AUTHORIZATION"]
+			credentials = auth_header.scan(/Basic (.+)/).flatten.first
+			#「ユーザ名:パスワード」というフォーマットで格納されている
+			username, password = Base64.decode64(credentials).split(":", 2)
+			p username
+			p password
+		end
+
 		return [
 			401,
 			{"WWW-Authenticate" => %Q(Basic realm="INPUT CREDENTIALS")},
